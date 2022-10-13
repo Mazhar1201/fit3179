fd60f4a598bd3d3464a28fe5ece9540a7187d030
Mazhar1201 <114343779+Mazhar1201@users.noreply.github.com>
2022-10-13 12:11:39 +0800

    helpers

---
+++ embed.js
@@ -0,0 +1,6 @@
+var vg_1 = "updatedmap.json";
+var vg_2 = "general.json";
+var vg_3 = "dumbbell.json";
+vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#gen", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#dumbb", vg_3).then(function(result) {}).catch(console.error);
