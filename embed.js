df7492bfb394c3c496ec0cd868f0971bb6c9524f
Mazhar1201 <114343779+Mazhar1201@users.noreply.github.com>
2022-10-17 21:47:07 +0800

    update content

--- embed.js
+++ embed.js
@@ -1,6 +1,17 @@
-var vg_1 = "updatedmap.json";
-var vg_2 = "general.json";
-var vg_3 = "dumbbell.json";
+var vg_1 = "map.json";
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
-vegaEmbed("#gen", vg_2).then(function(result) {}).catch(console.error);
-vegaEmbed("#dumbb", vg_3).then(function(result) {}).catch(console.error);
+/*var vg_2 = "summary.json";
+vegaEmbed("#summary", vg_2).then(function(result) {}).catch(console.error);*/
+
+var vg_2 = "scatter.json"
+vegaEmbed("#scat", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "cdot.json";
+vegaEmbed("#cdot", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "diverging.json";
+vegaEmbed("#divg", vg_4).then(function(result) {}).catch(console.error);
+
+var vg_5 = "multibar.json";
+vegaEmbed("#mbar", vg_5).then(function(result) {}).catch(console.error);
+
