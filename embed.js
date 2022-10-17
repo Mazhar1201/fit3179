bf4f03c07f4864963dc4994a8ca27d58e0c3a55e
Mazhar1201 <114343779+Mazhar1201@users.noreply.github.com>
2022-10-17 21:55:56 +0800

    update cleanup + description

--- embed.js
+++ embed.js
@@ -1,17 +1,11 @@
 var vg_1 = "map.json";
 vegaEmbed("#map", vg_1).then(function(result) {}).catch(console.error);
-/*var vg_2 = "summary.json";
-vegaEmbed("#summary", vg_2).then(function(result) {}).catch(console.error);*/
-
 var vg_2 = "scatter.json"
 vegaEmbed("#scat", vg_2).then(function(result) {}).catch(console.error);
-
 var vg_3 = "cdot.json";
 vegaEmbed("#cdot", vg_3).then(function(result) {}).catch(console.error);
-
 var vg_4 = "diverging.json";
 vegaEmbed("#divg", vg_4).then(function(result) {}).catch(console.error);
-
 var vg_5 = "multibar.json";
 vegaEmbed("#mbar", vg_5).then(function(result) {}).catch(console.error);
 
